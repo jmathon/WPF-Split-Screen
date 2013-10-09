27e824c2ca908ba7c1efd00672450c1f2f9211f0
Julien Mathon <julien.mathon@invenietis.com>
2013-10-09 14:45:26 +0200

    Add a little screen manager

--- WPFTry/App.xaml.cs
+++ WPFTry/App.xaml.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Threading;
 using WPFTry.ViewModels;
 using WPFTry.Views;
@@ -15,31 +17,46 @@ namespace WPFTry
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-        readonly GridViewModel _grid = new GridViewModel();
+        IList<MainWindow> _windows = new List<MainWindow>();
 
         public App()
         {
             InitializeComponent();
-            Window w = new MainWindow();
-            w.DataContext = _grid;
-            GridZone g = new GridZone( _grid.Current );
+
+            foreach( Screen s in Screen.AllScreens )
+                ConfigureScreen( s );
+
+        }
+
+        void ConfigureScreen( Screen screen )
+        {
+            GridViewModel grid = new GridViewModel();
+
+            MainWindow w = new MainWindow();
+            w.Left = screen.WorkingArea.Left;
+            w.Top = screen.WorkingArea.Top;
+
+            w.DataContext = grid;
+            GridZone g = new GridZone( grid.Current );
             Grid.SetColumn( g, 0 );
             Grid.SetRow( g, 0 );
             Grid myGrid = (Grid)w.Content;
             myGrid.Children.Add( g );
             w.Show();
+            w.WindowState = WindowState.Maximized;
+            Timer( grid );
 
-            Timer();
+            _windows.Add( w );
         }
 
-        private void Timer()
+        private void Timer( GridViewModel grid )
         {
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += delegate( object s, EventArgs args )
             {
-                _grid.SwitchCommand();
+                grid.SwitchCommand();
             };
             timer.Interval = new TimeSpan( 0, 0, 0, 0, Int32.Parse( ConfigurationManager.AppSettings["TimeToSwitch"] ) );
             timer.Start();
