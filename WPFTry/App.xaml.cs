7f812753513a372978664214949f70f4f6665b6c
Julien Mathon <julien.mathon@invenietis.com>
2013-10-10 17:47:44 +0200

    Screens highlight

--- WPFTry/App.xaml.cs
+++ WPFTry/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using System.Windows.Media;
 using System.Windows.Threading;
 using WPFTry.ViewModels;
 using WPFTry.Views;
@@ -19,6 +20,7 @@ namespace WPFTry
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        int selectedScreen = 0;
         IList<MainWindow> _windows = new List<MainWindow>();
 
         public App()
@@ -28,36 +30,52 @@ namespace WPFTry
             foreach( Screen s in Screen.AllScreens )
                 ConfigureScreen( s );
 
+            StartTimer( delegate( object s, EventArgs args )
+                {
+                    if( selectedScreen > 0 ) _windows[selectedScreen - 1].Background = new SolidColorBrush( System.Windows.Media.Color.FromRgb( 204, 204, 204 ) );
+                    else _windows[_windows.Count - 1].Background = new SolidColorBrush( System.Windows.Media.Color.FromRgb( 204, 204, 204 ) );
+
+                    _windows[selectedScreen].Background = new SolidColorBrush( System.Windows.Media.Color.FromRgb( 255, 153, 0 ) );
+
+                    if( selectedScreen < _windows.Count - 1 ) selectedScreen++;
+                    else selectedScreen = 0;
+                }
+            );
         }
 
         void ConfigureScreen( Screen screen )
         {
-            GridViewModel grid = new GridViewModel();
-
             MainWindow w = new MainWindow();
             w.Left = screen.WorkingArea.Left;
             w.Top = screen.WorkingArea.Top;
 
-            w.DataContext = grid;
+            w.Show();
+            w.WindowState = WindowState.Maximized;
+
+            _windows.Add( w );
+        }
+
+        void UseScreen( MainWindow w )
+        {
+            GridViewModel grid = new GridViewModel();
+
             GridZone g = new GridZone( grid.Current );
             Grid.SetColumn( g, 0 );
             Grid.SetRow( g, 0 );
             Grid myGrid = (Grid)w.Content;
             myGrid.Children.Add( g );
-            w.Show();
-            w.WindowState = WindowState.Maximized;
-            Timer( grid );
 
-            _windows.Add( w );
+            StartTimer( delegate( object s, EventArgs args )
+                {
+                    grid.SwitchCommand();
+                }
+            );
         }
 
-        private void Timer( GridViewModel grid )
+        private void StartTimer( EventHandler handler )
         {
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Tick += delegate( object s, EventArgs args )
-            {
-                grid.SwitchCommand();
-            };
+            timer.Tick += handler;
             timer.Interval = new TimeSpan( 0, 0, 0, 0, Int32.Parse( ConfigurationManager.AppSettings["TimeToSwitch"] ) );
             timer.Start();
         }
