1551e72f280c1f4d9b8aa4bcd16a60a39c9d9641
Julien Mathon <julien.mathon@invenietis.com>
2013-10-14 16:34:13 +0200

    Use a stack instead of a list to keep the history

--- src/WPFTry/ViewModels/GridViewModel.cs
+++ src/WPFTry/ViewModels/GridViewModel.cs
@@ -14,14 +14,13 @@ namespace WPFTry.ViewModels
     public class GridViewModel
     {
         readonly DispatcherTimer _timer = new DispatcherTimer();
-        IList<PanelViewModel> _panels = new List<PanelViewModel>();
-        PanelViewModel _current = null;
+        Stack<PanelViewModel> _panels = new Stack<PanelViewModel>();
 
         public PanelViewModel Current
         {
             get
             {
-                return _current;
+                return _panels.Peek();
             }
         }
 
@@ -30,16 +29,15 @@ namespace WPFTry.ViewModels
         public GridViewModel()
         {
             var m = new PanelViewModel();
-            _panels.Add( m );
-            _current = m;
+            _panels.Push( m );
         }
 
         public void SwitchCommand()
         {
-            Debug.Assert( _current != null );
+            Debug.Assert( Current != null );
             _timer.Tick += delegate( object s, EventArgs args )
             {
-                _current.Switch();
+		Current.Switch();
             };
             _timer.Interval = new TimeSpan( 0, 0, 0, 0, Int32.Parse( ConfigurationManager.AppSettings["TimeToSwitch"] ) );
             _timer.Start();
@@ -48,15 +46,14 @@ namespace WPFTry.ViewModels
 
         public void EnterCommand()
         {
-            Debug.Assert( _current != null );
+            Debug.Assert( Current != null );
             Debug.Assert( _panels.Count > 0 );
 
             if( (_panels.Count - 1) < MaxDeep )
             {
                 _timer.Stop();
-                var newPanel = _current.Enter();
-                _panels.Add( newPanel );
-                _current = newPanel;
+                var newPanel = Current.Enter();
+                _panels.Push( newPanel );
                 _timer.Start();
             }
             else
@@ -64,6 +61,14 @@ namespace WPFTry.ViewModels
                 _timer.Stop();
                 MessageBox.Show( "You have reached the max deep !", "Warning...", MessageBoxButton.OK, MessageBoxImage.Exclamation );
             }
+        }
+
+        public void ExitCommand()
+        {
+            Debug.Assert( Current != null );
+            Debug.Assert( _panels.Count > 0 );
+
+            var panelToRemove = _panels.Pop();
         }
     }
 }
